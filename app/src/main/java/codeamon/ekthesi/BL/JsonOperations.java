1b5d1655db32c1d8f91cc37c86931cb43eccf33a
OsmanKorcan <osman.korcan@gmail.com>
2016-05-01 21:26:15 +0300

    Json

---
+++ app/src/main/java/codeamon/ekthesi/BL/JsonOperations.java
@@ -0,0 +1,62 @@
+package codeamon.ekthesi.BL;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by OsmanKorcan on 1.5.2016.
+ */
+public class JsonOperations {
+public String categoryJson ="{\n" +
+        "\t\"categoryList\":[\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"1\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"0\",\n" +
+        "\t\"CategoryName\" :  \"Finans\",\n" +
+        "\t\"Type\" :\"1\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"2\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"0\",\n" +
+        "\t\"CategoryName\" :  \"Sigorta\",\n" +
+        "\t\"Type\" :\"1\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"3\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"0\",\n" +
+        "\t\"CategoryName\" :  \"Sigorta\",\n" +
+        "\t\"Type\" :\"1\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"4\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"3\",\n" +
+        "\t\"CategoryName\" :  \"Bayi Raporları\",\n" +
+        "\t\"Type\" :\"1\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"5\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"4\",\n" +
+        "\t\"CategoryName\" :  \"Komisyon Raporları\",\n" +
+        "\t\"Type\" :\"2\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"6\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"4\",\n" +
+        "\t\"CategoryName\" :  \"Prim Raporları\",\n" +
+        "\t\"Type\" :\"2\"\n" +
+        "\t},\n" +
+        "\t{\n" +
+        "\t\"Id\" : \"7\",\n" +
+        "\t\"ProjectId\" : \"1\",\n" +
+        "\t\"MainCategoryId\" : \"3\",\n" +
+        "\t\"CategoryName\" :  \"Poliçe Satış\",\n" +
+        "\t\"Type\" :\"1\"\n" +
+        "\t}\n" +
+        "]}";
+}
--- app/src/main/java/codeamon/ekthesi/MainActivity.java
+++ app/src/main/java/codeamon/ekthesi/MainActivity.java
@@ -3,11 +3,42 @@ package codeamon.ekthesi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import codeamon.ekthesi.BL.JsonOperations;
+import codeamon.ekthesi.Common.CategoryList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String json = new JsonOperations().categoryJson;
+        try  {
+            JSONObject jsonObj = new JSONObject(json);
+            JSONArray jsonArray = jsonObj.optJSONArray("categoryList");
+            for(int i=0; i < jsonArray.length(); i++){
+                JSONObject jsonlistOnj = jsonArray.getJSONObject(i);
+
+                int id = Integer.parseInt(jsonlistOnj.optString("Id").toString());
+                int ProjectId = Integer.parseInt(jsonlistOnj.optString("ProjectId").toString());
+                int MainCategoryId = Integer.parseInt(jsonlistOnj.optString("MainCategoryId").toString());
+                String CategoryName = jsonlistOnj.optString("CategoryName").toString();
+                int Type = Integer.parseInt(jsonlistOnj.optString("Type").toString());
+                CategoryList categoryList = new CategoryList(id,ProjectId,MainCategoryId,CategoryName,Type);
+
+            }
+
+        }
+        catch (JSONException ex){
+            ex.printStackTrace();      }
+
     }
+
 }
